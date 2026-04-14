28b88254eef6950f990e133f535ed194c8ff70c5
TearsOfGod <leviskariuki2@gmail.com>
2026-04-15 01:21:17 +0300

    Your commit message

--- index.js
+++ index.js
@@ -4,3 +4,22 @@
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
 };
+// Import datejs
+require('datejs');
+
+function combineUsers(...args) {
+  const combinedObject = {
+    users: []
+  };
+
+  for (let arr of args) {
+    combinedObject.users = [...combinedObject.users, ...arr];
+  }
+
+  combinedObject.merge_date = new Date().toString('M/d/yyyy');
+
+  return combinedObject;
+}
+
+// 👇 ADD THIS LINE (instead of module.exports = ...)
+module.exports.combineUsers = combineUsers;
